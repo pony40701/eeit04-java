2570ceab62b1214b0dc71cd8dba74214c754e304
pony40701 <pony40701@gmail.com>
2025-04-29 13:44:57 +0800

    202504291344

---
+++ PonyJava/src/tw/pony/tutor/Pony40.java
@@ -0,0 +1,10 @@
+package tw.pony.tutor;
+
+import java.io.File;
+
+public class Pony40 {
+	public static void main(String[] args) {
+		System.out.println(File.pathSeparator);
+		System.out.println(File.separator);
+	}
+}
