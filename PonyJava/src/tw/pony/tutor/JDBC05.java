1d71b48ab04290ca586a4cb16dbdf8821bf69297
pony40701 <pony40701@gmail.com>
2025-05-06 16:29:39 +0800

    05061629

---
+++ PonyJava/src/tw/pony/tutor/JDBC05.java
@@ -0,0 +1,34 @@
+package tw.pony.tutor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JDBC05 {
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost/pony";
+		Properties prop = new Properties();
+		prop.put("user", "root");
+		prop.put("password", "");
+		try {
+			Connection conn = DriverManager.getConnection(url, prop);
+
+			Statement stmt = conn.createStatement();
+			String sql = "SELECT * FROM cust";
+
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String cname = rs.getString("cname");
+				String tel = rs.getString("tel");
+				String birthday = rs.getString("birthday");
+				System.out.printf("%s:%s:%s:%s\n", id, cname, tel, birthday);
+			}
+			System.out.println("finish");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
